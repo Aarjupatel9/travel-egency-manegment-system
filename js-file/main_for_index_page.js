538aa9e03a64f22240228d4b8cf68c6d8cc4b475
Aarjupatel9 <98897856+Aarjupatel9@users.noreply.github.com>
2022-04-13 13:28:41 +0530

    final commit at 13:28 by aarju

--- js-file/main_for_index_page.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  var l_or_not = sessionStorage.getItem("s_id");
-  if (l_or_not === null) {
-    document.getElementById("user_image").src = "image/ser.ico";
-  } else {
-    document.getElementById("user_image").src = "image/user_icon2.png";
-  }
-};
